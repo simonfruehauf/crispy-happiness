cd5419f10dcf667375b5ff32db7cc42132136e3b
CrazyCookieBuddy <stockfisch97@gmail.com>
2021-12-29 12:58:13 +0100

    Objects, Entities & basic drawing

---
+++ Object.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crispy_happiness
+{
+
+    class Object
+    {
+        char icon;
+        int x;
+        int y;
+        ConsoleColor fc;
+        ConsoleColor bc;
+
+        public Object() { }
+
+
+        public Object(char _i, int _x, int _y, ConsoleColor _fc, ConsoleColor _bc = ConsoleColor.Black) //y is line
+        {
+            icon = _i;
+            x = _x;
+            y = _y;
+            bc = _bc;
+            fc = _fc;
+        }
+
+        public void move(int _x, int _y)
+        {
+            this.clear();
+            x += _x;
+            y += _y;
+        }
+        public void move(direction dir)
+        {
+            this.clear();
+            switch (dir)
+            {
+                case direction.North:
+                    y--;
+                    break;
+                case direction.NorthEast:
+                    y--;
+                    x++;
+                    break;
+                case direction.East:
+                    x++;
+                    break;
+                case direction.SouthWest:
+                    y++;
+                    x--;
+                    break;
+                case direction.South:
+                    y++;
+                    break;
+                case direction.SouthEast:
+                    y++;
+                    x++;
+                    break;
+                case direction.West:
+                    x--;
+                    break;
+                case direction.NorthWest:
+                    y--;
+                    x--;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.BackgroundColor = bc;
+            Console.ForegroundColor = fc;
+            Console.Write(icon);
+            Console.SetCursorPosition(0, 0); //reset Cursor
+
+        }
+
+        public void clear()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.ResetColor();
+            Console.Write(' ');
+            Console.SetCursorPosition(0, 0); //reset Cursor
+
+        }
+    }
+
+    class Entity:Object
+    {
+        int health;
+        public Entity() { }
+
+        public Entity(int _h, char _i, int _x, int _y, ConsoleColor _fc, ConsoleColor _bc = ConsoleColor.Black) : base(_i, _x, _y, _fc, _bc)
+        {
+            health = _h;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace crispy_happiness
+{
+    public class Write
+    {
+        public static void TypeLine(string line, int delay = 50, bool linebreak = false)
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                Console.Write(line[i]);
+                System.Threading.Thread.Sleep(delay);
+            }
+            if (linebreak)
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+    class Program
+    {
+        static List<Object> objects = new List<Object>(); 
+        
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            Entity player = new Entity(10, '@', 10, 10, ConsoleColor.Red);
+            Entity npc = new Entity(10, '@', 5, 10, ConsoleColor.Yellow);
+            objects.Add(player);
+            objects.Add(npc);
+
+            DrawObjects();
+            Console.ReadLine();
+            //Run In Circle
+            for (int i = 0; i < 8; i++)
+            {
+                Console.SetCursorPosition(0, 0);
+                player.move((direction)i);
+                DrawObjects();
+
+                System.Threading.Thread.Sleep(200);
+            }
+
+            Console.ReadLine();
+        }
+
+        static void DrawObjects()
+        {
+            DrawObjects(objects);
+        }
+        static void DrawObjects(List<Object> o)
+        {
+            foreach (Object ob in objects)
+            {
+                ob.clear();
+                ob.draw();
+
+            }
+        }
+
+    }
+}
